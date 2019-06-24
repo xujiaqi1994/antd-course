ac2dbacf99e69af88d6b0557454aba55a9060ef3
jiaqi.xu <jiaqi.xu@china.zhaogang.com>
2019-06-24 15:35:43 +0800

    框架

--- config/config.js
+++ config/config.js
@@ -1,12 +1,37 @@
 export default {
-plugins: [
-    ['umi-plugin-react', {
-      antd: true
-      // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
-    }],
-  ],	
+  singular: true,
+  // routes: [
+  //     {
+  //         path: '/',
+  //         component: 'HelloWorld',
+  //         Routes: ['./src/layout/BasicLayout']
+  //     },
+  // ],
   routes: [{
     path: '/',
-    component: './HelloWorld',
+    component: '../layout/BasicLayout',
+    routes: [
+      {
+        path: '/',
+        component: 'Helloworld',
+      },
+      {
+        path: '/helloworld',
+        component: 'Helloworld'
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+        ]
+      },
+    ]
   }],
+  plugins: [
+      ['umi-plugin-react', {
+          antd: true
+      }]
+  ],
 };
